78534b71566de18fe51f221ecfa8f7311909fbb6
satoshi <satoshi.kumano@kii.com>
2016-05-11 16:48:42 +0900

    Updated codes.

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -17,17 +17,37 @@ public class NewBehaviourScript : KiiInitializeBehaviour {
 	}
 
     string message = "hello.";
+    string username = "";
+    string password = "";
     void OnGUI() {
-        GUI.Label (new Rect (25, 25, 150, 30), message);
-        if (GUI.Button (new Rect (25, 25+50, 150, 30), "My")) {
+        GUI.Label (new Rect (25, 25, 150, 50), message);
+        username = GUI.TextField(new Rect(25, 100, 150, 50), username);
+        password = GUI.PasswordField(new Rect(25, 175, 150, 50), password, '*');
+
+        if (GUI.Button (new Rect (25, 250, 150, 50), "Login")) {
             message = "Login to cloud...";
 
-            KiiUser.LogIn("pass-1234","1234",(KiiUser user, Exception e) => {
+            KiiUser.LogIn(username, password,(KiiUser user, Exception e) => {
                 if (e != null) {
                     message = e.Message;
                     return;
                 }
                 message = "Login Success! id:" + user.ID;
+            });
+        }
+
+        if (GUI.Button(new Rect(25, 325, 150, 50), "Object"))
+        {
+            message = "Create object in Cloud...";
+            KiiObject obj = Kii.Bucket("TestBucket").NewKiiObject();
+            obj.Save((KiiObject obj2, Exception e2) =>
+            {
+                if (e2 != null)
+                {
+                    message = e2.Message;
+                    return;
+                }
+                message = "Save Object has been succeeded! id:" + obj2.Uri;
             });
         }
     }
